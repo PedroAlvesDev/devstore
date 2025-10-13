699b8322f1e08fe6dd661ab01b6464af125e3081
Seu Nome <139656791+PedroAlvesDev@users.noreply.github.com>
2025-10-13 14:22:58 -0300

    Page Home

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    qualities: [25, 50, 75, 100],
+
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
