10967c3c367d492f87d385639b4fdb60bc460a08
Seu Nome <139656791+PedroAlvesDev@users.noreply.github.com>
2025-10-13 13:58:52 -0300

    Page Home

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,8 +1,54 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
 
   return (
-    <h1>Hello World!</h1>
-  )
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
 
+      <Link href="/" className=" relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end">
+        <Image
+          src="/moletom-never-stop-learning.png"
+          width={860}
+          height={860}
+          quality={100}
+          alt="Moletom Never Stop Learning"
+        />
+        <div className="absolute  bottom-3/12 right-20 flex items-center border-2 text-sm font-semibold text-zinc-500 rounded-full gap-2 p-0.5 bg-[#0a0a0b]">
+          <span className="truncate max-w-[140px] text-zinc-100">Moletom Never Stop Learning</span>
+          <span className="font-semibold bg-violet-500 text-zinc-100 rounded-full shadow-lg px-4 py-2 gap-2">R$129</span>
+        </div>
+      </Link>
+
+      <Link href="/" className="relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end">
+        <Image
+          src="/moletom-ai-side.png"
+          width={860}
+          height={860}
+          quality={100}
+          alt="Moletom Ai Side"
+        />
+        <div className="absolute  bottom-3/12 right-20 flex items-center border-2 text-sm font-semibold text-zinc-500 rounded-full gap-2 p-0.5 bg-[#0a0a0b]">
+          <span className="truncate max-w-[140px] text-zinc-100">Moletom Ai Side</span>
+          <span className=" bg-violet-500 text-zinc-100 rounded-full shadow-lg px-4 py-2 gap-2">R$129</span>
+        </div>
+      </Link>
+
+      <Link href="/" className="relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end">
+        <Image
+          src="/camiseta-dowhile.png"
+          width={860}
+          height={860}
+          quality={100}
+          alt="Camiseta Dowhile"
+        />
+
+        <div className="absolute  bottom-3/12 right-20 flex items-center border-2 text-sm font-semibold text-zinc-500 rounded-full gap-2 p-0.5 bg-[#0a0a0b]">
+          <span className="truncate max-w-[140px] text-zinc-100">Camiseta Dowhile</span>
+          <span className="font-bold bg-violet-500 text-zinc-100 rounded-full shadow-lg px-4 py-2 gap-2">R$129</span>
+        </div>
+      </Link>
+
+    </div>
+  )
 }
