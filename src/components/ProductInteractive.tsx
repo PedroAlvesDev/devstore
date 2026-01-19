cc378fbd45627745c2b8ab863af522bcfcbc9bc8
Seu Nome <139656791+PedroAlvesDev@users.noreply.github.com>
2026-01-19 15:47:22 -0300

    Add Home page, Route

--- src/components/ProductInteractive.tsx
+++ src/components/ProductInteractive.tsx
@@ -1,31 +1,31 @@
-"use client";
+// "use client";
 
-import Image from "next/image";
-import ButtonAdd from "./buttonAdd";
+// import Image from "next/image";
+// import ButtonAdd from "./buttonAdd";
 
 
-export default function ProductInteractive() {
-    const handleAddToCart = (): void => {
-        alert("Clicou!!");
-    };
+// export default function ProductInteractive() {
+//     const handleAddToCart = (): void => {
+//         alert("Clicou!!");
+//     };
 
-    return (
-        <div className="relative z-0 col-span-3 row-span-3 rounded-lg bg-zinc-900 flex justify-center items-center">
-            <Image
-                src="/camiseta-dowhile.png"
-                width={860}
-                height={860}
-                quality={100}
-                alt="Camiseta Dowhile"
-                className="absolute z-10"
-            />
-            <ButtonAdd onClick={handleAddToCart}
-                className="relative -bottom-32 left-10 z-20">
-                <div className="flex items-center border-2 text-sm font-semibold text-zinc-500 rounded-full gap-2 p-0.5 bg-[#0a0a0b]">
-                    <span className="truncate max-w-[140px] text-zinc-100">Camiseta Dowhile</span>
-                    <span className="font-bold bg-violet-500 text-zinc-100 rounded-full shadow-lg px-4 py-2 gap-2">R$129</span>
-                </div>
-            </ButtonAdd>
-        </div>
-    );
-}
+//     return (
+//         <div className="group relative z-0 col-span-3 row-span-3 rounded-lg bg-zinc-900 flex justify-center items-center">
+//             <Image
+//                 src="/camiseta-dowhile.png"
+//                 width={860}
+//                 height={860}
+//                 quality={100}
+//                 alt="Camiseta Dowhile"
+//                 className="group-hover:scale-105 transition transform duration-500 absolute z-10"
+//             />
+//             <ButtonAdd onClick={handleAddToCart}
+//                 className="relative -bottom-32 left-10 z-20">
+//                 <div className="flex items-center border-2 text-sm font-semibold text-zinc-500 rounded-full gap-2 p-0.5 bg-[#0a0a0b]">
+//                     <span className="truncate max-w-[140px] text-zinc-100">Camiseta Dowhile</span>
+//                     <span className="font-bold bg-violet-500 text-zinc-100 rounded-full shadow-lg px-4 py-2 gap-2">R$129</span>
+//                 </div>
+//             </ButtonAdd>
+//         </div>
+//     );
+// }
